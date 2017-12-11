043b00a5c83ca6e2013e07a454b54c1d8b1c2309
rkota <ravikumar_kota@intuit.com>
2017-12-11 15:38:29 +0530

    run process in background

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Entered to start server applicaiton..."
-cd /tmp/app
-java -jar codepipeline-0.0.1-SNAPSHOT.jar
+cd /tmp/app/
+
+java -jar codepipeline-0.0.1-SNAPSHOT.jar > out.log 2>&1 &
 

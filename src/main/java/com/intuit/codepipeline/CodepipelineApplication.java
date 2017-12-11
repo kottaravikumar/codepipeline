f9ca7104ce9141a79c0943ef4c102a1edf8d7c7d
rkota <ravikumar_kota@intuit.com>
2017-12-11 12:12:54 +0530

    First commit spring boot

---
+++ src/main/java/com/intuit/codepipeline/CodepipelineApplication.java
@@ -0,0 +1,12 @@
+package com.intuit.codepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepipelineApplication.class, args);
+	}
+}

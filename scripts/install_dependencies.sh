c791868008a13647bc4d75ac02737c3a7217206e
rkota <ravikumar_kota@intuit.com>
2017-12-12 15:02:01 +0530

    install ruby and wget

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,7 +6,8 @@ yum update -y
 echo "Installing httpd"
 yum install -y httpd
 
-
+yum install -y ruby
+yum install -y wget
 wget https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto

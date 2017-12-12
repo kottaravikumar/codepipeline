45f76d8a698ef52f5206ba2563892764c90482fb
rkota <ravikumar_kota@intuit.com>
2017-12-12 14:29:32 +0530

    basic instll

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
+mkdir -p /tmp/app
+
 echo "Installing httpd"
 yum install -y httpd
-
-# Install codedeploy to Troubleshoot
-
-yum update -y
-wget https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent status
-
-# code deploy agent log file location.
-# /opt/codedeploy-agent/deployment-root/deployment-logs/codedeploy-agent-deployments.log
-
-# Install jdk 8
-
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm
-sudo yum install -y jdk-8u141-linux-x64.rpm
-
-
-mkdir -p /tmp/app

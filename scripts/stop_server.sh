a4107f9a783fd017eab5a01f8d1a0f7c87a234e8
rkota <ravikumar_kota@intuit.com>
2017-12-12 10:15:43 +0530

    Stopping server

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,4 +3,9 @@
 # Kill the existing process.
 set -e
 echo $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
-kill -9 $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
+
+{ # try
+    kill -9 $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
+} || { # catch
+    echo "No such process running"
+}

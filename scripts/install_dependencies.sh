511770ab783b7eb4ac64d76ad7edce9a3e3a855b
rkota <ravikumar_kota@intuit.com>
2017-12-12 14:48:15 +0530

    Start agent once installed..

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,4 +10,5 @@ yum install -y httpd
 wget https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
+sudo service codedeploy-agent start
 sudo service codedeploy-agent status

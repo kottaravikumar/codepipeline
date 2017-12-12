6de3b72e868aec8f970ea0f1d64b62b41456ce2b
rkota <ravikumar_kota@intuit.com>
2017-12-12 10:01:00 +0530

    Set ignore option

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Kill the existing process.
-echo "Proceess to kill"
+set -e
 echo $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
 kill -9 $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)

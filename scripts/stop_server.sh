24e65dc9280c83daa1789e242725631a5d748643
rkota <ravikumar_kota@intuit.com>
2017-12-12 10:54:56 +0530

    Fixed process id kill if not exisit

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
-
-# Kill the existing process.
+#!/bin/bash
 set -e
-echo $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
 
-{ # try
-    kill -9 $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
-} || { # catch
-    echo "No such process running"
-}
+proc_id=`ps aux | grep '[c]odepipeline' | awk '{print $2}'`
+
+echo "$proc_id of codepipeline process"
+
+if [[ -z "$proc_id" ]]; then
+    echo "No process found with codepipeline"
+else
+    echo "Found codepipeline process, kill the process to start a process with new revision."
+    echo "kill -9 " $proc_id
+    kill -9 $proc_id
+fi

28ec4d47214d5725b7f1178f1f1345a5f04a7511
rkota <ravikumar_kota@intuit.com>
2017-12-12 09:26:26 +0530

    Stop process command

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # Kill the existing process.
-kill -9 $(ps aux | grep 'codepipeline' | awk '{print $2}')
+echo "Proceess to kill"
+echo $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)
+kill -9 $(ps aux | grep 'codepipeline' | awk '{print $2}' | head -n 1)

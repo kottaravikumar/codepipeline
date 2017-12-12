61d88800d56c5c8ad3638b1157780a0a9349add2
rkota <ravikumar_kota@intuit.com>
2017-12-12 14:40:25 +0530

    Install codedeploy-agent

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,3 +5,9 @@ yum update -y
 
 echo "Installing httpd"
 yum install -y httpd
+
+
+wget https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
